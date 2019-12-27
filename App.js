648629619e89eb8f5888bc43ada7be0710c6aa71
hasnainalimj <mjhasnain15@gmail.com>
2019-12-26 20:02:02 -0800

    sync contacts from phone

--- App.js
+++ App.js
@@ -1,19 +1,51 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import * as Contacts from "expo-contacts";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loader: true,
+      contacts: []
+    };
+  }
+
+  componentDidMount() {
+    this.getContactsPermission();
+  }
+
+  getContactsPermission = async () => {
+    try {
+      const { status } = await Contacts.requestPermissionsAsync();
+      if (status === "granted") {
+        this.getContacts();
+      }
+    } catch (error) {
+      console.log("TCL: App -> getContactsPermission -> error", error);
+    }
+  };
+
+  getContacts = async () => {
+    try {
+      const { data } = await Contacts.getContactsAsync();
+      console.log("data", data);
+    } catch (error) {
+      console.log("TCL: App -> getContacts -> error", error);
+    }
+  };
+
+  render() {
+    return <View style={styles.container}></View>;
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
