d32e14b8a2b91d3b5d318b9642c87c9c5de560c8
hasnainalimj <mjhasnain15@gmail.com>
2019-12-26 20:07:59 -0800

    validate contacts rendering

--- App.js
+++ App.js
@@ -1,6 +1,13 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  ActivityIndicator
+} from "react-native";
 import * as Contacts from "expo-contacts";
+const { width, height } = Dimensions.get("window");
 
 export default class App extends React.Component {
   constructor(props) {
@@ -30,21 +37,54 @@ export default class App extends React.Component {
   getContacts = async () => {
     try {
       const { data } = await Contacts.getContactsAsync();
-      console.log("data", data);
+      if (data.length) {
+        this.setState({
+          loader: false,
+          contacts: data
+        });
+      } else {
+        this.setState({
+          loader: false,
+          contacts: []
+        });
+      }
     } catch (error) {
+      this.setState({
+        loader: false,
+        contacts: []
+      });
       console.log("TCL: App -> getContacts -> error", error);
     }
   };
 
   render() {
-    return <View style={styles.container}></View>;
+    const { loader, contacts } = this.state;
+    return (
+      <View>
+        {loader ? (
+          <View style={styles.loaderWrapper}>
+            <ActivityIndicator size="large" color="#000" />
+          </View>
+        ) : (
+          <React.Fragment>
+            {!contacts.length ? (
+              <View style={styles.loaderWrapper}>
+                <Text>No Contacts Found!</Text>
+              </View>
+            ) : (
+              <Text>Contacts Found</Text>
+            )}
+          </React.Fragment>
+        )}
+      </View>
+    );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
+  loaderWrapper: {
+    width,
+    height,
     alignItems: "center",
     justifyContent: "center"
   }
